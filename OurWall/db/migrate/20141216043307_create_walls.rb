64012b3cbd0fc7e79424f6676490029f76be6353
istorey <wrightstoreys@gmail.com>
2014-12-16 00:00:14 -0500

    define t

--- OurWall/db/migrate/20141216043307_create_walls.rb
+++ OurWall/db/migrate/20141216043307_create_walls.rb
@@ -1,7 +1,9 @@
 class CreateWalls < ActiveRecord::Migration
   def change
   	create_table :walls do |t|
-  	has_many :posts, :through => :neighborhood
+  	t.integer :neighborhood_id, null: false
+  	t.integer :image_id, null: false
+  	t.has_many :posts, :through => :neighborhood
   	end
   end
 end

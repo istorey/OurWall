3666cfa624f772da787307c74c0c25b7533668f1
istorey <wrightstoreys@gmail.com>
2014-12-16 00:08:19 -0500

    associations belong in models not migrations

--- OurWall/db/migrate/20141216043307_create_walls.rb
+++ OurWall/db/migrate/20141216043307_create_walls.rb
@@ -3,7 +3,6 @@ class CreateWalls < ActiveRecord::Migration
   	create_table :walls do |t|
   	t.integer :neighborhood_id, null: false
   	t.integer :image_id, null: false
-  	t.has_many :posts, :through => :neighborhood
   	end
   end
 end
--- OurWall/db/migrate/20141216043316_create_posts.rb
+++ OurWall/db/migrate/20141216043316_create_posts.rb
@@ -2,7 +2,6 @@ class CreatePosts < ActiveRecord::Migration
   def change
   	create_table :posts do |t|
   	t.text :image
-  	t.belongs_to :wall
   	end
   end
 end
